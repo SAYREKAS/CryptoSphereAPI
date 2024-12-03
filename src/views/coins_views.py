779cd0c78cea081582d9ef3a8a5550c47f501db8
sayrekas <chaotichoice@gmail.com>
2024-12-03 14:26:28 +0200

    New endpoints for coin transactions have been created:  - add_coin_for_user_endpoint  - get_all_coins_for_user_endpoint  - delete_coin_for_user_endpoint

--- src/views/coins_views.py
+++ src/views/coins_views.py
@@ -1,3 +1,26 @@
+"""Implementation of endpoints for working with coins in the user's portfolio"""
+
 from fastapi import APIRouter
 
+from src.schemas.crud_coin_schemas import NewUserCoinSchema, CoinInfoSchema, AllUserCoinsSchema
+from src.database.crud.crud_coin import add_coin_for_user, get_all_coins_for_user, delete_coin_for_user
+
 router = APIRouter()
+
+
+@router.post("/")
+async def add_coin_for_user_endpoint(coin_data: NewUserCoinSchema) -> CoinInfoSchema:
+    """Endpoint for adding a new user's coin"""
+    return await add_coin_for_user(coin_data=coin_data)
+
+
+@router.get("/")
+async def get_all_coins_for_user_endpoint(username: str) -> AllUserCoinsSchema:
+    """Endpoint for getting all user's coins"""
+    return await get_all_coins_for_user(username=username)
+
+
+@router.delete("/")
+async def delete_coin_for_user_endpoint(coin_data: NewUserCoinSchema) -> CoinInfoSchema:
+    """Endpoint for deleting a user's coin"""
+    return await delete_coin_for_user(coin_data=coin_data)
