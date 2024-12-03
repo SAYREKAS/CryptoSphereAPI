9f2e9d03d3331fff2489f6dcbac1fb06cdc663cd
sayrekas <chaotichoice@gmail.com>
2024-12-03 14:29:34 +0200

    New endpoints for working with user profiles have been create:  - create_user_endpoint  - read_all_users_endpoint  - read_user_by_username_endpoint  - delete_user_by_username_endpoint

--- src/views/users_views.py
+++ src/views/users_views.py
@@ -1,15 +1,32 @@
+"""Implementing endpoints for working with user profiles"""
+
 from fastapi import APIRouter
 
-from src.schemas.api_schemas import UserSchema, AddUserReportSchema
+from src.database.crud.crud_user import create_user, read_all_users, delete_user_by_username, read_user_by_username
+from src.schemas.crud_user_schemas import AllUsersSchema, DeleteUserResultSchema, NewUserInfoSchema, UserInfoSchema
 
 router = APIRouter()
 
 
-@router.get("/")
-def get_users():
-    pass
-
-
 @router.post("/")
-async def create_user(user: UserSchema) -> AddUserReportSchema:
-    return AddUserReportSchema(success=True if user else False)
+async def create_user_endpoint(user_data: NewUserInfoSchema) -> UserInfoSchema:
+    """Endpoint to add a new user to the database."""
+    return await create_user(user_data=user_data)
+
+
+@router.get("/")
+async def read_all_users_endpoint() -> AllUsersSchema:
+    """Endpoint to retrieve a list of usernames."""
+    return await read_all_users()
+
+
+@router.get("/{username}")
+async def read_user_by_username_endpoint(username: str) -> UserInfoSchema:
+    """Endpoint to retrieve a list of usernames."""
+    return await read_user_by_username(username)
+
+
+@router.delete("/{username}")
+async def delete_user_by_username_endpoint(username: str) -> DeleteUserResultSchema:
+    """Endpoint to remove a user from the database."""
+    return await delete_user_by_username(username)
