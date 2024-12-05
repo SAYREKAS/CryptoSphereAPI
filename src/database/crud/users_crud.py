5e6302e377bcf69df6352075d19b878b1efc1d8c
Oleh Ohiichuk <chaotichoice@gmail.com>
2024-12-05 12:36:21 +0200

    Now, when errors occur, we return an HTTPException with the appropriate status code and details. Refactoring and optimization.

--- src/database/crud/users_crud.py
+++ src/database/crud/users_crud.py
@@ -1,15 +1,19 @@
 """CRUD database operations"""
 
-import sqlalchemy
 from loguru import logger
+from pydantic import ValidationError
+from sqlalchemy import delete, select
+from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException, status
 
 from src.database.config import async_session
 from src.database.models import UsersORM
-from src.schemas.crud_users_schemas import UserInfoSchema, NewUserInfoSchema, AllUsersSchema, DeleteUserResultSchema
+from src.schemas.crud_users_schemas import UserInfoSchema, NewUserSchema, AllUsersSchema
 
 
-async def create_user(user_data: NewUserInfoSchema) -> UserInfoSchema:
+async def create_user(user_data: NewUserSchema) -> UserInfoSchema:
     """Add a new user to the database."""
+
     async with async_session() as session:
         try:
             new_user = UsersORM(**user_data.model_dump())
@@ -18,29 +22,28 @@ async def create_user(user_data: NewUserInfoSchema) -> UserInfoSchema:
 
             logger.info(f"User created: username='{user_data.username}', email='{user_data.email}'.")
             return UserInfoSchema(
-                success=True,
-                message=f"User '{user_data.username}' successfully created.",
                 username=user_data.username,
                 email=user_data.email,
             )
-        except sqlalchemy.exc.IntegrityError as e:
-            logger.warning(f"Integrity error for user '{user_data.username}': {e}")
-            return UserInfoSchema(
-                success=False,
-                message="Username or email already exists.",
-                username=user_data.username,
-                email=user_data.email,
-            )
+        except IntegrityError:
+            logger.warning(f"Integrity error for user '{user_data.username}'.")
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Username or email already exists.")
+
+        except ValidationError as e:
+            logger.warning(f"Validation error for user '{user_data.username}': {e}")
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Validation error. Check input data.")
 
 
 async def read_all_users() -> AllUsersSchema:
     """Retrieve all registered users."""
+
     async with async_session() as session:
         try:
-            users_query = await session.execute(sqlalchemy.select(UsersORM.username))
+            users_query = await session.execute(select(UsersORM.username))
             users = users_query.scalars().all()
             logger.info(f"Retrieved {len(users)} users.")
             return AllUsersSchema(users=users)
+
         except Exception as e:
             logger.error(f"Error retrieving users: {e}")
             return AllUsersSchema(users=[])
@@ -48,47 +51,42 @@ async def read_all_users() -> AllUsersSchema:
 
 async def read_user_by_username(username: str) -> UserInfoSchema:
     """Retrieve a user by their username."""
+
     async with async_session() as session:
-        user_query = await session.execute(sqlalchemy.select(UsersORM).where(UsersORM.username == username))
+        user_query = await session.execute(select(UsersORM).where(UsersORM.username == username))
+
         user = user_query.scalar_one_or_none()
         if not user:
             logger.warning(f"User '{username}' not found.")
-            return UserInfoSchema(
-                success=False,
-                message=f"User '{username}' not found.",
-            )
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User '{username}' not found.")
+
         logger.info(f"User '{username}' retrieved.")
-        return UserInfoSchema(
-            success=True,
-            message=f"User '{username}' found.",
-            username=user.username,
-            email=user.email,
-        )
+        return UserInfoSchema(username=user.username, email=user.email)
 
 
-async def delete_user_by_username(username: str) -> DeleteUserResultSchema:
+async def delete_user_by_username(username: str) -> UserInfoSchema:
     """Delete a user from the database."""
+
     async with async_session() as session:
         try:
-            result = await session.execute(sqlalchemy.delete(UsersORM).where(UsersORM.username == username))
-            if result.rowcount == 0:
-                logger.warning(f"User '{username}' not found for deletion.")
-                return DeleteUserResultSchema(
-                    success=False,
-                    message=f"User '{username}' not found.",
-                    username=username,
-                )
-            await session.commit()
-            logger.info(f"User '{username}' deleted.")
-            return DeleteUserResultSchema(
-                success=True,
-                message=f"User '{username}' deleted successfully.",
-                username=username,
+            result = await session.execute(
+                delete(UsersORM).where(UsersORM.username == username).returning(UsersORM.username, UsersORM.email)
             )
+
+            user = result.fetchone()
+            if user is None:
+                logger.warning(f"User '{username}' not found for deletion.")
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail=f"User '{username}' not found for deletion."
+                )
+
+            await session.commit()
+
+            name, email = user
+
+            logger.info(f"User deleted: username='{name}', email='{email}'.")
+            return UserInfoSchema(username=name, email=email)
+
         except Exception as e:
             logger.error(f"Error deleting user '{username}': {e}")
-            return DeleteUserResultSchema(
-                success=False,
-                message="An error occurred while deleting the user.",
-                username=username,
-            )
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="General error, try again later.")
